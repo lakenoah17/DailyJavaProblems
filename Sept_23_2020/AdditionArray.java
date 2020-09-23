acaf7f0ef019b6029e2094374b20b76580219da8
Noah Shields <noahshields5@gmail.com>
2020-09-23 19:00:39 -0400

    Solution 1 done

---
+++ Sept_23_2020/AdditionArray.java
@@ -0,0 +1,48 @@
+package Sept_23_2020;
+
+public class AdditionArray {
+    //This problem was recently asked by Google.
+    //
+    //Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+    //
+    //For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+    //
+    //Bonus: Can you do this in one pass?
+
+    public static void main(String[] args){
+        int[] nums = {10, 15, 3, 7};
+        System.out.println(solution1(nums, 17));
+        System.out.println(solution1(nums, 25));
+        System.out.println(solution1(nums, 11));
+    }
+
+    //Checks all possibilities of adding nums together
+    //Params:
+    //  nums - array of numbers that can be added together
+    //  numToAddTo - sum that two numbers in the array must equal
+    //Returns: whether two numbers in array add together to equal
+    //the specified sum or not
+    public static boolean solution1(int[] nums, int numToAddTo){
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] == numToAddTo){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //Checks all possibilities of adding nums together
+    //Params:
+    //  nums - array of numbers that can be added together
+    //  numToAddTo - sum that two numbers in the array must equal
+    //Returns: whether two numbers in array add together to equal
+    //the specified sum or not
+    public static boolean solution2(int[] nums, int numToAddTo){
+        for (int i = 0; i < nums.length; i++){
+
+        }
+        return false;
+    }
+}
